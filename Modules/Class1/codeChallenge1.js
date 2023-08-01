93c8393d0feb9f0cbce3221234378caea9ccb0a7
M-H-J-J <matthew.h.j.janzen@gmail.com>
2023-07-31 17:02:06 -0700

    First assignment

---
+++ Modules/Class1/codeChallenge1.js
@@ -0,0 +1,12 @@
+let name = 'Matthew';
+let age = 31;
+let isStudent = false;
+let favouriteColours = ['Green', 'White', 'Purple']
+let person = {
+    name,
+    age,
+    isStudent,
+    favouriteColours
+}
+
+console.log(person)
