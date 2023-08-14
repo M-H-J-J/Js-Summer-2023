01743467cc76edcc08f5227ababc626b0ab207c8
M-H-J-J <matthew.h.j.janzen@gmail.com>
2023-08-14 13:44:27 -0700

    Second Assignment

---
+++ Modules/Class1/codeChallenge2.js
@@ -0,0 +1,48 @@
+//Arithmetic Operators:
+//1
+let num1 = 4
+let num2 = 7
+let product = num1 + num2
+console.log(product)
+//2
+let radius = 12
+let area = [radius *= 3.14159] * radius
+console.log(area)
+
+//Assignment Operators:
+//1
+let count = 5
+count += 2
+console.log(count)
+//2
+let price = 10
+price *= 2.5
+console.log(price)
+
+//Comparison Operators:
+//1
+let age1 = 26
+let age2 = 31
+console.log(age1 > age2)
+//2
+let name = 'Crum'
+console.log(name !== 'John Doe')
+
+//Logical Operators:
+//1
+let isMember = true
+let hasCoupon = false
+console.log(isMember || hasCoupon)
+//2
+let num = 11
+console.log(num = !0)
+
+//Conditional (Ternary) Operator:
+//1
+let age = 15
+let canVote = age >= 18 ? "You are old enough to vote" : "You are not old enough to vote";
+console.log(canVote)
+//2
+let productPrice = 25
+let productOnSale = productPrice < 25 ? "The product is on sale" : "The product is not on sale"
+console.log(productOnSale)
